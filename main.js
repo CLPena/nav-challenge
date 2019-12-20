7ac4dacccc72106125d615d912b08d13da4f03c5
Cristina Pena <cristinalpena@gmail.com>
2019-12-20 10:17:10 -0700

    Add page toggle functionality, refactor some JS and CSS

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 var header = document.querySelector('header');
 var buttons = document.querySelectorAll('.btn');
+var allCoursesPage = document.querySelector('.all-courses');
+var collectionsPage = document.querySelector('.collections');
+var wishlistPage = document.querySelector('.wishlist');
+var archivedPage = document.querySelector('.archived');
+var pages = document.querySelectorAll('.page');
 
-header.addEventListener('click', addBorder);
+header.addEventListener('click', changeView);
+
+function changeView(){
+  addBorder();
+  removeContent();
+  addContent();
+}
 
 function addBorder(){
   if (event.target.classList.contains('btn')){
@@ -9,5 +20,25 @@ function addBorder(){
       buttons[i].classList.remove('border');
     }
     event.target.classList.add('border');
+  }
+}
+
+function removeContent(){
+  if (event.target.classList.contains('btn')){
+    for (var i = 0; i < pages.length; i++) {
+      pages[i].classList.add('invisible');
+    }
+  }
+}
+
+function addContent(){
+  if (event.target.classList.contains('all-courses-btn')){
+    allCoursesPage.classList.remove('invisible');
+  } else if (event.target.classList.contains('collections-btn')){
+    collectionsPage.classList.remove('invisible');
+  } else if (event.target.classList.contains('wishlist-btn')){
+    wishlistPage.classList.remove('invisible');
+  } else if (event.target.classList.contains('archived-btn')){
+    archivedPage.classList.remove('invisible');
   }
 }
